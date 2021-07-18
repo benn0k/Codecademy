ed635ef9e0c207290e111b05756d43b218b0c266
Ryan Bennek <rbennek.rb@gmail.com>
2021-07-18 15:28:11 -0500

    Create HTML Project

--- Codecademy/JavaScript/JS Scratch/JSScratch.js
+++ Codecademy/JavaScript/JS Scratch/JSScratch.js
@@ -169,9 +169,9 @@ switch (randomNumber) {
 
 let raceNumber = Math.floor(Math.random() * 1000);
 var regesteredEarly = false;
-let runnerAge= 27;
+let runnerAge = 27;
 
-if (runnerAge > 18 && regesteredEarly === true){
+if (runnerAge > 18 && regesteredEarly === true) {
   raceNumber += 1000;
 }
 
@@ -179,9 +179,9 @@ if (runnerAge > 18 && regesteredEarly === true) {
   console.log(`Your race is at 9:30AM and you racer number is ${raceNumber}.`);
 } else if (runnerAge > 18 && regesteredEarly === false) {
   console.log(`Late adults run at 11:00AM. Your racer number is ${raceNumber}.`);
-} else if (runnerAge < 18){
+} else if (runnerAge < 18) {
   console.log(`Youth registrants run at 12:30 PM. Your racer number is ${raceNumber}.`);
-} else if (runnerAge === 18){
+} else if (runnerAge === 18) {
   console.log(`Please see the regestration desk `);
 }
 
