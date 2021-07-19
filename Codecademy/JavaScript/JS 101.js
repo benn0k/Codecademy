b3bc81df9eb000f9d85847d42d4aaf4ce7350bfd
Ryan Bennek <rbennek.rb@gmail.com>
2021-07-19 12:09:37 -0500

    Cleanup

--- Codecademy/JavaScript/JS 101.js
+++ Codecademy/JavaScript/JS 101.js
@@ -5,3 +5,8 @@ console.log() //logs to console
 //Used to shorthand code, common uses include://
 console.log('Codecademy'.toUpperCase());
 //this will log the string but uppercased to console//
+let myName = 'Ryan';
+
+if (myName === 'Ryan'){
+
+}
