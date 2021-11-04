71b4be7d6bedf4f5554a559e3fd839bf07ec6fde
Ryan Bennek <rbennek.rb@gmail.com>
2021-11-04 17:10:46 -0500

    update NODE

--- Codecademy/JavaScript/JS Scratch/NODE.js
+++ Codecademy/JavaScript/JS Scratch/NODE.js
@@ -1,18 +1,22 @@
 //declare what you want to translate
-let input = "I'm stinky!";
+let input = "I'm stinky!".toLowerCase();
 //declare a "vowels" array where we'll put our vowels
 const vowels = ["a", "e", "i", "o", "u"];
 //create resultArray to store vowels from input
 let resultArray = [];
-
+//first loop iterates through input
 for (let i = 0; i < input.length; i++) {
-  //debug console.log(i);
+  //debug console.log(input[i]);
+
+  //second loop iterates through vowels while iterating through input
   for (let e = 0; e < vowels.length; e++) {
-    //debug console.log(e);
+    // console.log(vowels[e]);
+    //if the current input equals the current vowels
     if (input[i] === vowels[e]) {
+      //push to resultArray
       resultArray.push(vowels[e]);
     }
   }
 }
-
+//log that shit, i, i expected
 console.log(resultArray);
