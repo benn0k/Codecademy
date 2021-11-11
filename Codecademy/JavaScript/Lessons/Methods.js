84257cc887ec7dfbbf9eda69d1f01b781bd83e5f
Ryan Bennek <rbennek.rb@gmail.com>
2021-11-10 20:52:19 -0600

    add Methods commit

--- Codecademy/JavaScript/Lessons/Methods.js
+++ Codecademy/JavaScript/Lessons/Methods.js
@@ -1,17 +1,34 @@
+//! Methods
+//? Method
+//Think of them like arrays for code snippets
+const alienShip = {
+  invade() {
+    console.log(
+      "Hello! We have come to dominate your planet. Instead of Earth, it shall be called New Xaculon."
+    );
+  },
+};
+
+alienShip.invade(); // Prints 'Hello! We have come to dominate your planet. Instead of Earth, it shall be called New Xaculon.'
+
+//* Example--->
 let retreatMessage =
   "We no longer wish to conquer your planet. It is full of dogs, which we do not care for.";
 
 // Write your code below
 //? Below the retreatMessage variable in the code editor, create an alienShip object. It should contain a method .retreat() which will console.log() the retreatMessage
 
+// create object
 const alienShip = {
+  //first method
   retreat() {
     console.log(retreatMessage);
   },
+  //second method
   takeOff() {
     console.log("Spim... Borp... Glix... Blastoff!");
   },
 };
-
+//call methods
 alienShip.retreat();
 alienShip.takeOff();
