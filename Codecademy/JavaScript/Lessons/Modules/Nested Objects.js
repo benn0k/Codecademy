6ad02ed266ce7163a4c75fe0f2e630bee043ab7b
benn0k <rbennek.rb@gmail.com>
2021-11-14 14:23:50 -0600

    test
    1

--- Codecademy/JavaScript/Lessons/Modules/Nested Objects.js
+++ Codecademy/JavaScript/Lessons/Modules/Nested Objects.js
@@ -95,7 +95,6 @@ let spaceship = {
   "Fuel Type": "Turbo Fuel",
   homePlanet: "Earth",
 };
-
 // Write your code below
 
 //?Write a function greenEnergy() that has an object as a parameter and sets that object’s 'Fuel Type' property to 'avocado oil'.
