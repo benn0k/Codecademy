8d27319456e4b4425f2a28dbfbc935b2daf8e4bd
Ryan Bennek <rbennek.rb@gmail.com>
2021-11-27 09:45:32 -0600

    create mealMaker

--- Codecademy/JavaScript/Small JS Projects/mealMaker.js
+++ Codecademy/JavaScript/Small JS Projects/mealMaker.js
@@ -1,1 +1,17 @@
-*--
+const menu = {
+  //Define Object courses, create empty array for each property
+  courses: {
+    appetizers: [],
+    mains: [],
+    desserts: [],
+  },
+  // create getters and setters for each property
+  get appetizers() {},
+  set appetizers(appetizerIn) {},
+
+  get mains() {},
+  set mains(mainsIn) {},
+
+  get desserts() {},
+  set desserts(dessertsIn) {},
+};
