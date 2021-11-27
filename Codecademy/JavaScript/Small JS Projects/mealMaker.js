bf7d0039d2340b5b2fec29577ac11a7f299691c0
Ryan Bennek <rbennek.rb@gmail.com>
2021-11-27 10:20:21 -0600

    update mealMaker

--- Codecademy/JavaScript/Small JS Projects/mealMaker.js
+++ Codecademy/JavaScript/Small JS Projects/mealMaker.js
@@ -1,6 +1,6 @@
 const menu = {
   //Define Object courses, create empty array for each property
-  courses: {
+  _courses: {
     appetizers: [],
     mains: [],
     desserts: [],
@@ -14,4 +14,48 @@ const menu = {
 
   get desserts() {},
   set desserts(dessertsIn) {},
+  //creates getter for _courses
+  get _courses() {
+    //return apps, mains, desserts
+    return {
+      appetizers: this.appetizers,
+      mains: this.mains,
+      desserts: this.desserts,
+    };
+  },
+  //create method to add dish. Takes in name, price
+  addDishToCourse(courseName, dishName, coursePrice) {
+    //create object dish that includes name and price
+    const dish = {
+      name: courseName,
+      dish: dishName,
+      price: coursePrice,
+    };
+    //push object to
+    this._courses[courseName].push(dish);
+  },
+  //create function to get random Dish
+  getRandomDishFromCourse: function (courseName) {
+    //get array of coursename from _courses
+    const dishes = this._courses[courseName];
+    // return a dish from dishes using random index
+    const randomIndex = Math.floor(Math.random() * dishes.length);
+  },
+  //create function to generate random meal
+  generateRandomMeal: function () {
+    const appetizer = this.getRandomDishFromCourse("appetizers");
+    const mains = this.getRandomDishFromCourse("mains");
+    const desserts = this.getRandomDishFromCourse("desserts");
+    const totalPrice = appetizer.price + mains.price + desserts.price;
+
+    return `Your meal is ${appetizer.name}, ${mains.name}, ${desserts.name}. The price is $${totalPrice},`;
+  },
 };
+//Add dishes to course
+menu.addDishToCourse("appetizers", "Caesar Salad", 4.0);
+menu.addDishToCourse("mains", "Steak", 4);
+menu.addDishToCourse("dessert", "Cake", 4);
+//debug and check
+let meal = menu.generateRandomMeal();
+
+console.log(meal);
