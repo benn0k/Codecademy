0e21b0421356f1a057c1ad5377c0741d7485e07e
Ryan Bennek <rbennek.rb@gmail.com>
2021-12-06 17:09:42 -0600

    update

--- Codecademy/JavaScript/Lessons/Higher-Order Functions/FunctionsAsData.js
+++ Codecademy/JavaScript/Lessons/Higher-Order Functions/FunctionsAsData.js
@@ -1,1 +1,24 @@
-console.log('test')
+//! Functions as Data
+const announceThatIAmDoingImportantWork = () => {
+    console.log("I’m doing very important work!");
+  };
+  
+  //can be written as 
+  
+  const busy = announceThatIAmDoingImportantWork;
+   
+  busy(); // This function call barely takes any space!
+  
+  // Write your code below
+  
+  const checkThatTwoPlusTwoEqualsFourAMillionTimes = () => {
+    for(let i = 1; i <= 1000000; i++) {
+      if ( (2 + 2) != 4) {
+        console.log('Something has gone very wrong :( ');
+      }
+    }
+  }
+  
+  const is2p2 = checkThatTwoPlusTwoEqualsFourAMillionTimes;
+  
+  is2p2();
