2cde849dd67e8b236d621d627a17df3f98fb71e0
Ryan Bennek <rbennek.rb@gmail.com>
2021-12-05 20:42:25 -0600

    Add Higher order functions

---
+++ Codecademy/JavaScript/Lessons/Higher-Order Functions/FunctionsAsData.js
@@ -0,0 +1,15 @@
+//! Functions as Data
+
+const announceThatIAmDoingImportantWork = () => {
+  console.log('I’m doing very important work!');
+};
+
+const checkThatTwoPlusTwoEqualsFourAMillionTimes = () => {
+  for (let i = 1; i <= 1000000; i++) {
+    if (2 + 2 != 4) {
+      console.log('Something has gone very wrong :( ');
+    }
+  }
+};
+
+// Write your code below
